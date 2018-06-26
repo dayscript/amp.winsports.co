f13c5a0c79ce53be1d89633e52382f4d3a8d9eb5
aacevedoday <aacevedo@dayscript.com>
2018-06-26 08:53:40 -0500

    validate

--- resources/views/article-amp.blade.php
+++ resources/views/article-amp.blade.php
@@ -77,7 +77,7 @@
       @endswitch
     @endif
     {{-- Type Image --}}
-    @if( isset($content->field_image->und[0]->realpath) && !isset($content->field_is_video_article->und[0]->value ) || !$content->field_is_video_article->und[0]->value )
+    @if( isset($content->field_image->und[0]->realpath) && !isset($content->field_is_video_article->und[0]->value ) )
       <amp-img alt="A view of the sea"
         src="{{$content->field_image->und[0]->realpath}}"
         width="{{$content->field_image->und[0]->width}}"

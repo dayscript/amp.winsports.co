6494562207439a89b7e7aa272723e366a32fea2f
yruizday <yruiz@dayscript.com>
2018-12-19 14:57:53 -0500

    update multimedia

--- routes/web.php
+++ routes/web.php
@@ -10,25 +10,22 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', function () {return view('welcome');});
 Route::get('aws', 'Controller@aws');
 
 /*
  * Routes
  */
-
 Route::resource('article', 'ArticleController');
 
 /*
  * Routes for AMP
  */
 Route::amp('{type}/noticias/{title}', ['as' => 'article', 'uses' => 'ArticleController@showAmp']);
-Route::amp('{type}/multimedia/galeria-goles/{title}', ['as' => 'multimediaForGalleryGoals', 'uses' => 'ArticleController@showAmpGalleryGoalsMultimedia']);
-Route::amp('{type}/multimedia/galeria-imagenes/{title}', ['as' => 'multimediaForGalleryNonCategory', 'uses' => 'ArticleController@showAmpGalleryMultimediaNonCategory']);
 Route::amp('{type}/{category}/noticias/{title}', ['as' => 'articleForCategory', 'uses' => 'ArticleController@showAmpForCategory']);
+Route::amp('{type}/multimedia/galeria-goles/{title}', ['as' => 'multimediaForGalleryGoals', 'uses' => 'ArticleController@showAmpGalleryGoalsMultimedia']);
+Route::amp('{type}/multimedia/galeria-compactos/{title}', ['as' => 'multimediaForGalleryGoals', 'uses' => 'ArticleController@showAmpGalleryGoalsMultimedia']);
+Route::amp('{type}/multimedia/galeria-imagenes/{title}', ['as' => 'multimediaForGalleryNonCategory', 'uses' => 'ArticleController@showAmpGalleryMultimediaNonCategory']);
 Route::amp('{type}/{category}/multimedia/videos/{title}', ['as' => 'multimediaForCategory', 'uses' => 'ArticleController@showAmpMultimedia']);
 Route::amp('{type}/{category}/multimedia/galeria-imagenes/{title}', ['as' => 'nada', 'uses' => 'ArticleController@showAmpGallery']);
+Route::amp('{type}/{category}/multimedia/galeria-compactos/{title}', ['as' => 'nada', 'uses' => 'ArticleController@showAmpGallery']);
